fd84aea968adeb610186047f0ee760ffd57acdae
Ryan Daum <ryan.daum@gmail.com>
2023-07-02 16:08:00 -0400

    Add initial implementation of optimistic lock
      * will be used for the optimistic lock coupled concurrent version of the tree

--- rart/src/utils/mod.rs
+++ rart/src/utils/mod.rs
@@ -1,3 +1,11 @@
+use std::cell::Cell;
+use std::marker::PhantomData;
+use std::sync::MutexGuard;
+
 pub mod bitarray;
 pub mod bitset;
+pub mod optimistic_lock;
 pub mod u8_keys;
+
+pub type PhantomUnsync = PhantomData<Cell<()>>;
+pub type PhantomUnsend = PhantomData<MutexGuard<'static, ()>>;
